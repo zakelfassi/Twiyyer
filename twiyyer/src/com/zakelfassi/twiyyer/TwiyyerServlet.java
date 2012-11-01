7d1dccb29e0c0946c7d9f45a4ac9a90d9d07ca40
Zak Elfassi <zakelfassi@gmail.com>
2012-11-01 16:12:19 +0000

    Add Users Service.

--- twiyyer/src/com/zakelfassi/twiyyer/TwiyyerServlet.java
+++ twiyyer/src/com/zakelfassi/twiyyer/TwiyyerServlet.java
@@ -2,14 +2,27 @@ package com.zakelfassi.twiyyer;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 public class TwiyyerServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Hello, Twiyyer !");
+//    	resp.setContentType("text/plain");
+//      resp.getWriter().println("Hello, Twiyyer !");
+    	
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+
+        if (user != null) {
+            resp.setContentType("text/plain");
+            resp.getWriter().println("Hello, " + user.getNickname());
+        } else {
+            resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+//            Goes to simulated login screen on local dev server; redirects to Google accounts sign-in when run on GAE.
+        }
     }
 }
